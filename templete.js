8e200cb1e4bc2531708775d823dad8492de5683c
Cola <bcad1591@gmail.com>
2022-04-18 16:51:57 +0900

    템플릿, gitignore

---
+++ templete.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath =
+  process.platform === 'linux'
+    ? '/dev/stdin'
+    : path.join(__dirname, `${__dirname.split('\\').pop()}.txt`);
+
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const solution = () => {};
+
+console.log(solution());
+
+/* --------------------------------- */
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {}) //
+  .on('close', () => {}); //
